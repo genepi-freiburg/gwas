1b21ba346d8c54fc8e3f4e171c0ebb07f40e2109
Matthias Wuttke <matthias.wuttke@uniklinik-freiburg.de>
2013-08-19 16:16:26 +0200

    rename / refactor

--- cleaning-pipeline/04-plot-het-miss.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-SD=2
-MISS=0.03
-IN="${RESULT_DIR}/${SOURCE_NAME}"
-OUT="${RESULT_DIR}/imiss-vs-het-SD${SD}-M${MISS}.pdf"
-
-log "Plot imiss-vs-het diagram, SD = ${SD}, MISS = ${MISS}"
-Rscript ${SCRIPT_DIR}/04-imiss-vs-het.R ${IN} ${OUT} ${SD} ${MISS} 2>&1 >/dev/null
--- cleaning-pipeline/cleaning-pipeline.sh
+++ cleaning-pipeline/cleaning-pipeline.sh
@@ -19,24 +19,24 @@ echo >> ${SUMMARY_FILE}
 
 ### PIPELINE
 log "filter non-founder SNPs"
-. ${SCRIPT_DIR}/00-no-founders.sh
+. ${SCRIPT_DIR}/cleaning/00-no-founders.sh
 
 log "perform sex check"
-. ${SCRIPT_DIR}/01-sexcheck.sh
+. ${SCRIPT_DIR}/cleaning/01-sexcheck.sh
 
 log "perform missingness analysis"
-. ${SCRIPT_DIR}/02-missingness.sh
+. ${SCRIPT_DIR}/cleaning/02-missingness.sh
 
 log "perform heterozygosity analysis"
-. ${SCRIPT_DIR}/03-heterozygosity.sh
+. ${SCRIPT_DIR}/cleaning/03-heterozygosity.sh
 
 log "plot heterozygosity and missingness"
-. ${SCRIPT_DIR}/04-plot-het-miss.sh
+. ${SCRIPT_DIR}/cleaning/04-plot-het-miss.sh
 
 log "perform cryptic relatedness analysis"
-. ${SCRIPT_DIR}/05-relatedness.sh
+. ${SCRIPT_DIR}/cleaning/05-relatedness.sh
 
 log "perform PCA"
-. ${SCRIPT_DIR}/06-pca.sh
+. ${SCRIPT_DIR}/cleaning/06-pca.sh
 
 log "cleaning finished"
