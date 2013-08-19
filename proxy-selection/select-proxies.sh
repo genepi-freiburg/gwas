8f0dfe26d93f6b95ea6fb5ab53416c344ed90340
Matthias Wuttke <matthias.wuttke@uniklinik-freiburg.de>
2013-08-19 12:25:53 +0200

    Script for proxy search

---
+++ proxy-selection/select-proxies.sh
@@ -0,0 +1,46 @@
+ORIGINAL_SNP=${1}
+WORKING_DIR=${2}
+if [ "${WORKING_DIR}" == "" ]
+then
+	WORKING_DIR=/tmp/proxysnps
+fi
+
+if [ "${ORIGINAL_SNP}" == "" ]
+then
+	echo "Usage: ${0} snpid [working_dir]"
+	exit 9
+fi
+
+rm -f ${WORKING_DIR}/proxy-snps.*
+
+POP=EUR
+KGP_DATA=/data/gwas/1kgp/merge/${POP}/1kgp-${POP}
+WINDOW_SIZE=250
+PLINK=/usr/lib/plink/plink
+
+echo -n "Determine chromosome for ${ORIGINAL_SNP}: " >&2
+CHR=`grep ${ORIGINAL_SNP} ${KGP_DATA}.bim | cut -f 1`
+echo "${ORIGINAL_SNP} is on chromosome ${CHR}." >&2
+
+echo "Searching for proxies for ${ORIGINAL_SNP} (window size ${WINDOW_SIZE} kb)" >&2
+mkdir -p ${WORKING_DIR}
+echo "Using working directory ${WORKING_DIR}" >&2
+cd ${WORKING_DIR}
+
+KGP_CHR_DATA="/data/gwas/1kgp/final/${POP}/1kgp-${POP}-chr${CHR}"
+
+${PLINK} --noweb \
+	--bfile ${KGP_CHR_DATA} \
+	--r2 \
+	--ld-window-kb ${WINDOW_SIZE} \
+	--ld-window-r2 0.2 \
+	--ld-window 999999 \
+	--ld-snp ${ORIGINAL_SNP} \
+	--out ${WORKING_DIR}/proxy-snps >&2
+
+# sortieren und ausgeben
+echo "CHR POS_1 RSID_1 POS_2 RSID_2 R2 DIST"
+tail -n+2 ${WORKING_DIR}/proxy-snps.ld | awk \
+	'function abs(x) { return ((x < 0.0) ? -x : x) } 
+         { print $1 "\t" $2 "\t" $3 "\t" $5 "\t" $6 "\t" $7 "\t" abs($5-$2) }' \
+	| sort -k 6rn,7n
