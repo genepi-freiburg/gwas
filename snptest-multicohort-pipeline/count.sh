401655ba21d3b12303258ee517673d2494762fc3
Matthias Wuttke <matthias.wuttke@uniklinik-freiburg.de>
2013-08-28 17:53:11 +0200

    Finish pipeline

--- snptest-multicohort-pipeline/count.sh
+++
@@ -1,7 +0,0 @@
-FNS=`find adjusted -name *.gwas`
-for FN in $FNS
-do
-	echo "$FN 1E-5 1E-6" 
-	cat $FN | cut -f 9 | awk '{ if ($1 < 1E-5) print }' | wc -l
-	cat $FN | cut -f 9 | awk '{ if ($1 < 1E-6) print }' | wc -l
-done
