38ec8e894b19c479edde454c4a9a310284c21622
Matthias Wuttke <matthias@wuttke.eu>
2014-05-16 10:45:39 +0200

    improvements (model, N)

--- probabel/gw-coxph.R
+++ probabel/gw-coxph.R
@@ -1,10 +1,17 @@
 #### ARGUMENTS
 
-args = commandArgs(trailingOnly=T)
-in_dose = args[1]
-in_pheno = args[2]
-in_maf_file = args[3] #"whites_ckid-chr22.snp-freqs.txt"
-out_ass = args[4]
+if (!exists("dont_read_commandline")) {
+  args = commandArgs(trailingOnly=T)
+  in_dose = args[1]
+  in_pheno = args[2]
+  in_maf_file = args[3] #"whites_ckid-chr22.snp-freqs.txt"
+  out_ass = args[4]
+}
+
+#### JIT
+
+require(compiler)
+enableJIT(3)
 
 #### GENOTYPES
 
@@ -30,36 +37,57 @@ stopifnot(ncol(geno) == nrow(pheno))
 rownames(geno) = map$RSID
 colnames(geno) = pheno$IID
 
+print(paste("got", nsnp, "SNPs and", nind, "individuals"))
+
 #### SURVIVAL TIME AND NULL MODEL
 
 library("methods") # for "is"
+library("splines")
 library("survival")
 kidney_surv = with(pheno, Surv(SURV_TIME, EVENT))
 
+all_covars = names(pheno)
+my_covars = all_covars[4:length(all_covars)]
+null_form_str = paste("kidney_surv ~", paste(my_covars, collapse=" + "))
+null_form = as.formula(null_form_str)
+
+snp_form_str = paste("kidney_surv ~ snp + pheno$", paste(my_covars, collapse=" + pheno$"), sep="")
+snp_form = as.formula(snp_form_str)
+
 print("construct zero model")
-model_zero = coxph(kidney_surv ~ pheno$SEX + pheno$AGE_BASELINE + pheno$EGFR_EXP_BASELINE)
+print(null_form)
+model_zero = coxph(null_form, data=pheno)
 loglik_zero = model_zero$loglik[2]
 print(model_zero)
 
 #### SNP LOOP
 
-result = data.frame(snp=map$RSID, beta=NA, se=NA, chisq_lrt=NA, pval_lrt=NA, chisq_wald=NA, chisq_lrt=NA, info=NA)
+#make_model = function(snp) { coxph(kidney_surv ~ snp + pheno$SEX + pheno$AGE_BASELINE + pheno$EGFR_EXP_BASELINE) }
+make_model = function(snp) { 
+  coxph(as.formula(snp_form_str)) 
+}
+cmpfun(make_model)
+
+result = data.frame(snp=map$RSID, beta=NA, se=NA, chisq_lrt=NA, pval_lrt=NA, chisq_wald=NA, pval_wald=NA, n=NA, info=NA)
 
 options(warn=-1)
 for (i in 1:nsnp) {
-  if ((i %% 1000) == 0) { print(paste("processing SNP", i)) }
+  if ((i %% 1000) == 0) { 
+    print(paste("processing SNP ", i, "/", nsnp, " (", round(i*100/nsnp,1), "%)", sep=""))
+  }
+  
   snp = geno[i,]
   if (any(is.na(snp))) {
-    print(paste("WARNING: Genotype data contains NA values; LRT chisq inaccurate for SNP", map[i, "RSID"]))
+    print(paste("WARNING: Genotype data contains NA values; LRT chisq/pval inaccurate for SNP", map[i, "RSID"]))
   }
-
-  make_model = function() { coxph(kidney_surv ~ snp + pheno$SEX + pheno$AGE_BASELINE + pheno$EGFR_EXP_BASELINE) }
-  model = tryCatch(make_model(), warning=function(w){w}, error=function(e){e})
+  
+  model = tryCatch(make_model(snp), warning=function(w){w}, error=function(e){e})
   if (is(model, "warning")) { 
     status = paste("WARNING:", model)
     model = NULL
   } else if (is(model, "error")) {
     status = paste("ERROR:", model)
+    print(status)
     model = NULL
   } else {
     status = "OK"
@@ -82,8 +110,9 @@ for (i in 1:nsnp) {
     result[i, "pval_lrt"] = pval_lrt
     result[i, "chisq_wald"] = chisq_wald
     result[i, "pval_wald"] = pval_wald
+    result[i, "n"] = model$n
   } 
-
+  
   result[i, "info"] = status
 }
 options(warn=0)
@@ -94,11 +123,12 @@ print(paste("reading frequencies from", in_maf_file))
 maf = read.table(in_maf_file, h=T)
 print("frequencies read")
 
-maf = data.frame(RSID=maf$RSID,MAF=maf$MAF)
+maf = data.frame(RSID=maf$RSID, minor_allele_freq=maf$MAF,coded_allele_freq=(2*maf$AA+maf$AB)/(2*(maf$AA+maf$AB+maf$BB)))
 stopifnot(nrow(maf) == nrow(result))
 
-map = cbind(map, maf$MAF)
-colnames(map)[6] = "MAF"
+map = cbind(map, maf$minor_allele_freq, maf$coded_allele_freq)
+colnames(map)[6] = "minor_allele_freq"
+colnames(map)[7] = "coded_allele_freq"
 
 #### CHECK AND MERGE RESULT
 result = cbind(map, result)
--- probabel/gwcoxph2gwas.R
+++ probabel/gwcoxph2gwas.R
@@ -1,15 +1,19 @@
-args <- commandArgs(trailingOnly = TRUE)
-infn = args[1]
-outfn = args[2]
-freqfn = args[3]
-chrnum = args[4]
+if (!exists("dont_read_commandline")) {
+  args <- commandArgs(trailingOnly = TRUE)
+  infn = args[1]
+  outfn = args[2]
+  freqfn = args[3]
+  chrnum = args[4]
+}
 
-print("read gw-coxph result")
-indat = read.table(infn, h=T)
-summary(probable)
+print(paste("reading gwas result file", infn))
+indat = read.table(infn, h=T, sep="\t")
+print("result file read")
+summary(indat)
 
 #name A1 A2 Freq1 MAF Quality Rsq n Mean_predictor_allele chrom position beta_SNP_add sebeta_SNP_add chi2_SNP
 #"SNPID", "RSID", "POS", "ALL1", "ALL2"   MAF beta se chisq_lrt pval_lrt chisq_wald pval_wald
+# minor_allele_freq, coded_allele_freq
 #SNP     chr     position        coded_all       noncoded_all    strand_genome   beta    SE      pval    AF_coded_all    HWE_pval        callrate        n_total imputed used_for_imp    oevar_imp
 out = data.frame(
 	SNP=indat$RSID,
@@ -21,10 +25,10 @@ out = data.frame(
 	beta=indat$beta,
 	SE=indat$se,
 	pval=indat$pval_wald,
-	AF_coded_all=-1,            		# -1, wird ersetzt
+	AF_coded_all=indat$coded_allele_freq,
 	HWE_pval=1,
 	callrate=-1,		              	# -1, wird ersetzt
-	n_total=-1,				# wird ersetzt
+	n_total=indat$n,
 	imputed=0, 				# wird ersetzt
 	used_for_imp=1, 			# wird ersetzt
 	oevar_imp=1 				# haben wir derzeit nicht, aber "information" measure
@@ -33,8 +37,9 @@ summary(out)
 
 rm(indat)
 
-print("read SNP freqs")
+print(paste("reading SNP frequencies", freqfn))
 freqs = read.table(freqfn, h=T)
+print("SNP frequencies read")
 summary(freqs)
 
 # SNPID RSID chromosome position A_allele B_allele minor_allele major_allele AA AB BB       AA_calls AB_calls BB_calls MAF HWE missing missing_calls information
@@ -42,27 +47,21 @@ summary(freqs)
 # rs35416799 rs35416799 NA 16869887 G A A G                                  240 30 0       240 30 0                   0.055556 -0 0 0 1
 
 freq_short = data.frame(
-        SNP=freqs$RSID,
-	A1FREQ=(2 * freqs$AA + freqs$AB)/(2*(freqs$AA+freqs$AB+freqs$BB)),
+  SNP=freqs$RSID,
 	CALLRA=1-freqs$missing,
 	IMPUTD=(freqs$SNPID == "---"),
-	NTOT=freqs$AA+freqs$AB+freqs$BB,
 	INFORM=freqs$information)
 rm(freqs)
 
 final = merge(out, freq_short, all.x=T, by="SNP")
 
-final$AF_coded_all = final$A1FREQ
 final$callrate = final$CALLRA
 final$imputed = ifelse(final$IMPUTD, 1, 0)
 final$used_for_imp = ifelse(final$IMPUTD, 0, 1)
 final$oevar_imp = final$INFORM
-final$n_total = final$NTOT
 
-final$A1FREQ = NULL
 final$CALLRA = NULL
 final$IMPUTD = NULL
 final$INFORM = NULL
-final$NTOT   = NULL
 
 write.table(final, outfn, row.names=F, col.names=T, quote=F, sep="\t")
