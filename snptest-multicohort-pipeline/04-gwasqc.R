5c5870a5d96bd0b13404c8b988b2442ff35945be
Matthias Wuttke <matthias.wuttke@uniklinik-freiburg.de>
2018-07-24 11:44:09 +0200

    new path

--- snptest-multicohort-pipeline/04-gwasqc.R
+++ snptest-multicohort-pipeline/04-gwasqc.R
@@ -1,3 +1,4 @@
-library(GWAtoolbox, lib.loc="/home/wuttke/bin/GWAtoolbox")
+#library(GWAtoolbox, lib.loc="/home/wuttke/bin/GWAtoolbox")
+library(GWAtoolbox, lib.loc="/data/programs/bin/gwas/R/x86_64-unknown-linux-gnu-library/3.2")
 gwasqc("gwasqc.in.txt")
 
